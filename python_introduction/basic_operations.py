7ad166b8ee79a9dea65b0f23bb686550dc656a48
Stennis1 <stennismith@gmail.com>
2024-12-12 14:55:18 +0000

    first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,11 @@
+
+number1 = 10
+number2 = 5
+
+sum_numbers = number1 + number2
+substract_numbers = number1 + number2
+multiply_numbers = number1 + number2
+
+print(f"Addition of {number1} and {number2} is {sum_numbers}")
+print(f"Substraction of {number1} and {number2} is {substract_numbers}")
+print(f"Multiplication of {number1} and {number2} is {multiply_numbers}")
