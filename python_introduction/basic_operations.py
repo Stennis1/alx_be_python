bc0eaa595b68eda1e61240e158b7dce8263244c1
Stennis1 <stennismith@gmail.com>
2024-12-12 15:03:51 +0000

    Update arithmetic operations

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -3,8 +3,8 @@ number1 = 10
 number2 = 5
 
 sum_numbers = number1 + number2
-substract_numbers = number1 + number2
-multiply_numbers = number1 + number2
+substract_numbers = number1 - number2
+multiply_numbers = number1 * number2
 
 print(f"Addition of {number1} and {number2} is {sum_numbers}")
 print(f"Substraction of {number1} and {number2} is {substract_numbers}")
