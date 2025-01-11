10bfb1f5166e2e0b838683bef44b4fb35dcd7e02
Stennis1 <stennismith@gmail.com>
2025-01-11 00:40:27 +0000

    Update shopping list manager script

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -14,19 +14,38 @@ def main():
         display_menu()
         choice = input("Enter your choice: ").strip()
         
-        # Logic for adding, reving, viewing, and exiting the shopping list
-        if choice == "1":
-            item = input("Enter name of item: ").strip()
-            shopping_list.append(item)
-        
-        elif choice == "2":
-            item = input("Enter name of item: ").strip()
-            shopping_list.remove(item)
-        
-        elif choice == "3":
-            print(shopping_list)
+        # Checks for implementation of Choice Input as a number
+        if not choice.isdigit():
+            print("Invalid choice. Please select again")
+            continue # Skip the rest of the loop and start from the top 
 
-        elif choice == "4":
+        choice = int(choice)
+        
+        # Logic for adding, reving, viewing, and exiting the shopping list
+        if choice == 1:
+            item = input("Enter name of item: ").strip()
+            if item:
+                shopping_list.append(item)
+                print(f"{item} has been added to the list.")
+            else:
+                print("Item name cannot be empty. Please add an item.")
+
+        elif choice == 2:
+            item = input("Enter name of item: ").strip()
+            if item not in shopping_list:
+                print("Item not found in the shopping list.")
+            else:                
+                shopping_list.remove(item)
+                print(f"{item} has been removed from the list.")
+        
+        elif choice == 3:
+            if shopping_list:
+                for idx, item in enumerate(shopping_list, start=1):
+                    print(f"{idx}. {item}")
+            else:
+                print("Shopping list is empty.")
+
+        elif choice == 4:
             print("Goodbye!")
             break
 
