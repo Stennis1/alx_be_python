0c08cad9a591e6fcb704d1547cb4c390ffaccccf
Stennis1 <stennismith@gmail.com>
2025-01-14 14:35:02 +0000

    Bank Class

---
+++ programming_paradigm/Exercises/test.py
@@ -0,0 +1,16 @@
+"""
+
+Practice Exercises
+Exercise 1: Understanding the Importance of Testing
+
+Instruction:
+
+Write a small Python function (e.g., a function to calculate the square of a number) and intentionally introduce a bug (e.g., incorrect calculation logic).
+Exercise 2: Writing Basic Unit Tests
+
+Instruction:
+
+Use the unittest module in Python to create a test case for the buggy function from Exercise 1.
+Write test methods to check different scenarios (e.g., valid input, invalid input) and verify expected behavior.
+
+"""
