f0d0a1b87ca6fd6a9b3b4cebffd0f7ea79498703
Stennis1 <stennismith@gmail.com>
2025-03-03 00:23:19 +0000

    Add class_static_methods_demo script.

--- oop/class_static_methods_demo.py
+++ oop/class_static_methods_demo.py
@@ -1,1 +1,13 @@
 #  A python script
+
+class Calculator:
+    calculation_type = "Arithmetic Operations"
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @classmethod
+    def multiply(cls, a, b):
+        print(f"Calculation type: {cls.calculation_type}")
+        return a * b
